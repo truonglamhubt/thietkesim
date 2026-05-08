2b18fbb01556ce9625581965ec8e4227ab17d19b
truonglamhubt <124659299+truonglamhubt@users.noreply.github.com>
2026-05-08 15:34:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -110,4 +110,5 @@ def fb_webhook():
 def home(): return "Bot Chuyên Gia Thietkesim v3.0 Live!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
